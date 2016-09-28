3b48518be0d37ce775934ce64a34cdb53d66b89b
pietrobenito <petebennett1982@gmail.com>
2016-09-28 11:14:52 +0100

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,7 +1,7 @@
 <?
 // This is a template for a PHP scraper on morph.io (https://morph.io)
 // including some code snippets below that you should find helpful
-
+$Scr = new Scraperwiki;
 // require 'scraperwiki.php';
 // require 'scraperwiki/simple_html_dom.php';
 //
