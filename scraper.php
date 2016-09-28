b4934f797b6212479314beb4e62672546acdeb1c
pietrobenito <petebennett1982@gmail.com>
2016-09-28 11:18:16 +0100

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,13 +1,13 @@
 <?
 // This is a template for a PHP scraper on morph.io (https://morph.io)
 // including some code snippets below that you should find helpful
-$Scr = new Scraperwiki;
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
+require 'scraperwiki.php';
+require 'scraperwiki/simple_html_dom.php';
 //
 // // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
+$html = scraperwiki::scrape('https://scholar.google.co.uk/scholar?hl=en&q="Copyright+Policy"');
 //
+var_dump($html);
 // // Find something on the page using css selectors
 // $dom = new simple_html_dom();
 // $dom->load($html);
