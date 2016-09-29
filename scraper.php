f087a831519f07f6438dcd7c33a37a87d1391c4e
Peter Bennett <peter.bennett@glasgow.ac.uk>
2016-09-29 13:33:31 +0100

    Working

--- scraper.php
+++ scraper.php
@@ -1,16 +1,51 @@
-<?
+<?php
+header('Content-Type: application/json');
 // This is a template for a PHP scraper on morph.io (https://morph.io)
 // including some code snippets below that you should find helpful
-require 'scraperwiki.php';
-require 'scraperwiki/simple_html_dom.php';
-//
+
+require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/vendor/openaustralia/scraperwiki/scraperwiki.php';
+require __DIR__ . '/vendor/openaustralia/scraperwiki/simple_html_dom.php';
+
+$url = 'https://scholar.google.co.uk/scholar?hl=en&as_sdt=1,5&as_vis=1&q=%22Copyright+Legislation%22&scisbd=1';
 // // Read in a page
-$html = scraperwiki::scrape('https://scholar.google.co.uk/scholar?hl=en&q="Copyright+Policy"');
+$html = scraperwiki::scrape($url);
 //
 // // Find something on the page using css selectors
 $dom = new simple_html_dom();
 $dom->load($html);
-print_r($dom->find("div.gs_r"));
+// print_r($dom->find('div.gs_rs', 0));
+$posts = [];
+$i = "0";
+foreach ($dom->find("h3.gs_rt a") as $ln) {
+    // print $ln . "<hr>\n";
+    $posts[$i]['title'] = utf8_encode($ln->innertext);
+    $i++;
+}
+$i="0";
+foreach ($dom->find("h3.gs_rt a") as $ln) {
+    // print $ln . "<hr>\n";
+    $posts[$i]['link'] = utf8_encode($ln->href);
+    $i++;
+}
+$i="0";
+foreach ($dom->find("div.gs_a") as $ln) {
+    // print $ln . "<hr>\n";
+    $posts[$i]['author'] = utf8_encode($ln->innertext);
+    $i++;
+}
+$i="0";
+foreach ($dom->find("div.gs_rs") as $ln) {
+    // print $ln . "<hr>\n";
+    // $escapedText = htmlspecialchars($ln->innertext);
+    $posts[$i]['details'] = utf8_encode($ln->innertext);
+    $i++;
+}
+
+$json = json_encode($posts, JSON_PRETTY_PRINT | JSON_NUMERIC_CHECK, 2);
+
+echo $json;
+
 //
 // // Write out to the sqlite database using scraperwiki library
 // scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
